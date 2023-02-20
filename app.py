25c48759802dcf3ccd702783f4bde1afc18329c8
BADRI KRISHNAN A <65157349+LiteBrick204@users.noreply.github.com>
2023-02-20 09:50:03 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,38 @@
+import numpy as np
+import os
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from flask import Flask,render_template,request
+
+import cv2
+import numpy as np
+from playsound import playsound
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+model=load_model("ffd_model.h5")
+  
+@app.route('/')
+def index():
+    return render_template("index.html")
+text=''
+@app.route('/predict',methods=['GET','POST'])
+def upload():
+    if request.method=='POST':
+        f=request.files['image']
+        filepath=os.path.join('static/',f.filename)
+        f.save(filepath)
+        img=image.load_img(filepath,target_size=(,))
+        x=image.img_to_array(img)
+        x = np.expand_dims(x,axis=0)
+        pred = model.predict(x)
+        y = int(pred[0][0])
+        if(pred==1):
+            text='OK'
+        else:
+            text='NO FIRE'
+    return text
+
+if __name__=='__main__':
+    app.run(debug=False)
